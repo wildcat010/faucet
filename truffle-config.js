158e7203506e7f3261dae8ed8d7a9d4a83e29c3b
Benoit Bourgeon <benoit.bourgeon.ext@primetals.com>
2026-01-21 18:03:07 +0100

    repo update

---
+++ truffle-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+    },
+  },
+
+  mocha: {
+    // timeout: 100000
+  },
+
+  compilers: {
+    solc: {
+      version: "0.8.21",
+      // docker: true,
+      // settings: {
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    },
+  },
+};
